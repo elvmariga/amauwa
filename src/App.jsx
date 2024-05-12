b1d326c94654bb4a52b3bb952cc3ddef2968dbf7
Elvis Mariga <37267714+elvmariga@users.noreply.github.com>
2024-05-12 18:24:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import mobileplant from "./mobileplant.svg";
 function App() {
   return (
     <div style={{ height: "100vh", display: "grid", justifyContent: "center" }}>
-      {/* <Hero />
+      <Hero />
       <About />
 
       <WhatWeOffer />
@@ -28,12 +28,12 @@ function App() {
       <OurProcess />
       <Shop />
       <Testimony />
-      <Contact /> */}
+      <Contact />
       {/* <div>WE ARE COMING SOON!!</div> */}
-      <div className="image-container ">
+{/*       <div className="image-container ">
         <img className="tablet-image" src={plant} alt="" />
         <img className="mobile-image" src={mobileplant} alt="" />
-      </div>
+      </div> */}
       <div></div>
     </div>
   );
